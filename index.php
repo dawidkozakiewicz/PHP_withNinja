430fb03fe80270006b12ea57e767c5cf8880bd86
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-03-20 22:50:11 +0100

    rendering data to a browser

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 
 	// connect to the database
-	$conn = mysqli_connect('localhost', 'dawid', 'Potaspotas1', 'ninja_pizza');
+	$conn = mysqli_connect('localhost', 'shaun', 'test1234', 'ninja_pizza');
 
 	// check connection
 	if(!$conn){
@@ -23,8 +23,6 @@
 	// close connection
 	mysqli_close($conn);
 
-	print_r($pizzas);
-
 
 ?>
 
@@ -32,6 +30,30 @@
 <html>
 	
 	<?php include('templates/header.php'); ?>
+
+	<h4 class="center grey-text">Pizzas!</h4>
+
+	<div class="container">
+		<div class="row">
+
+			<?php foreach($pizzas as $pizza){ ?>
+
+				<div class="col s6 md3">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+							<div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+						</div>
+						<div class="card-action right-align">
+							<a class="brand-text" href="#">more info</a>
+						</div>
+					</div>
+				</div>
+
+			<?php } ?>
+
+		</div>
+	</div>
 
 	<?php include('templates/footer.php'); ?>
 
