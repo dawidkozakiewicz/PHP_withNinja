fc5a435413d1fdf9779e3ffabef91d27b23cf5b1
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-03-24 22:29:00 +0100

    saving data to a database

--- index.php
+++ index.php
@@ -1,70 +1,58 @@
-<?php
+<?php 
 
-// connect to the database
-$conn = mysqli_connect('localhost', 'dawid', 'Potaspotas1', 'ninja_pizza');
+	include('config/db_connect.php');
 
-// check connection
-if (!$conn) {
-	echo 'Connection error: ' . mysqli_connect_error();
-}
+	// write query for all pizzas
+	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
-// write query for all pizzas
-$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+	// get the result set (set of rows)
+	$result = mysqli_query($conn, $sql);
 
-// get the result set (set of rows)
-$result = mysqli_query($conn, $sql);
+	// fetch the resulting rows as an array
+	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-// fetch the resulting rows as an array
-$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	// free the $result from memory (good practise)
+	mysqli_free_result($result);
 
-// free the $result from memory (good practise)
-mysqli_free_result($result);
-
-// close connection
-mysqli_close($conn);
+	// close connection
+	mysqli_close($conn);
 
 
 ?>
 
 <!DOCTYPE html>
 <html>
+	
+	<?php include('templates/header.php'); ?>
 
-<?php include('templates/header.php'); ?>
+	<h4 class="center grey-text">Pizzas!</h4>
 
-<h4 class="center grey-text">Pizzas!</h4>
+	<div class="container">
+		<div class="row">
 
-<div class="container">
-	<div class="row">
+			<?php foreach($pizzas as $pizza): ?>
 
-		<?php foreach ($pizzas as $pizza) : ?>
-
-			<div class="col s6 md3">
-				<div class="card z-depth-0">
-					<div class="card-content center">
-						<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-						<ul class="grey-text">
-							<?php foreach (explode(',', $pizza['ingredients']) as $ing) : ?>
-								<li><?php echo htmlspecialchars($ing); ?></li>
-							<?php endforeach; ?>
-						</ul>
-					</div>
-					<div class="card-action right-align">
-						<a class="brand-text" href="#">more info</a>
+				<div class="col s6 m4">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+							<ul class="grey-text">
+								<?php foreach(explode(',', $pizza['ingredients']) as $ing): ?>
+									<li><?php echo htmlspecialchars($ing); ?></li>
+								<?php endforeach; ?>
+							</ul>
+						</div>
+						<div class="card-action right-align">
+							<a class="brand-text" href="#">more info</a>
+						</div>
 					</div>
 				</div>
-			</div>
 
-		<?php endforeach; ?>
+			<?php endforeach; ?>
 
-		<?php if (count($pizzas) >= 3) : ?>
-			<p>There is more than 3 pizza</p>
-		<?php else : ?>
-			<p>There are fewer than 3 pizzas</p>
-		<?php endif; ?>
-
+		</div>
 	</div>
-</div>
 
-<?php include('templates/footer.php'); ?>
+	<?php include('templates/footer.php'); ?>
 
 </html>
